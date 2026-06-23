1759c4139c172191801e996008b1e60780d5835a
Tahsin <tahsinkhan12n@gmail.com>
2026-06-23 10:23:39 +0600

    some more changes

---
+++ csharp/CsharpProjects/TestProject2/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] fraudulentIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+foreach (string ids in fraudulentIDs)
+{
+    if (ids.StartsWith("B"))
+    {
+        Console.WriteLine(ids);
+    }
+}
