44cf2f009f86bea43861ff01c3a9361faf2e0e72
Tahsin <tahsinkhan12n@gmail.com>
2026-06-23 12:18:43 +0600

    some more changes :)

---
+++ csharp/update.cs
@@ -0,0 +1,17 @@
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+int letterCount = 0;
+
+foreach (char letter in message)
+{
+    if (letter == 'o')
+    {
+        letterCount++;
+    } 
+}
+string newMessage = new String(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
