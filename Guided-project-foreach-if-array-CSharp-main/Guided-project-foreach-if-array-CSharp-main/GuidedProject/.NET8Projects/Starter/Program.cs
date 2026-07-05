61fda793e0f6f32489b26861be739e006f05c9a4
Tahsin <tahsinkhan12n@gmail.com>
2026-07-05 15:42:50 +0600

    some changes

--- Guided-project-foreach-if-array-CSharp-main/Guided-project-foreach-if-array-CSharp-main/GuidedProject/.NET8Projects/Starter/Program.cs
+++ Guided-project-foreach-if-array-CSharp-main/Guided-project-foreach-if-array-CSharp-main/GuidedProject/.NET8Projects/Starter/Program.cs
@@ -4,76 +4,65 @@
 int currentAssignments = 5;
 
 int[] sophiaScores = [90, 86, 87, 98, 100];
-/*
 int[] andrewScores = [92, 89, 81, 96, 90];
 int[] emmaScores = [90, 85, 87, 98, 68];
 int[] loganScores = [90, 95, 87, 88, 96];
 
-
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
-
-
-decimal andrewScore;
-decimal emmaScore;
-decimal loganScore;
-
-/*
-
-
-foreach (int score in andrewScores)
-{
-    andrewSum += score;
-}
-
-andrewScore = (decimal)andrewSum / currentAssignments;
-
-foreach (int score in emmaScores)
-{
-    emmaSum += score;
-}
-
-emmaScore = (decimal)emmaSum / currentAssignments;
-
-foreach (int score in loganScores)
-{
-    loganSum += score;
-}
-
-loganScore = (decimal)loganSum / currentAssignments;
-*/
-
-
 string[] studentName = ["Sophia", "Andrew", "Emma", "Logan"];
 int[] studentScores = new int[10];
+
+string currentStudentletterGread = "";
+
+Console.WriteLine("Student\t\tGrade\n");
 
 foreach (string name in studentName)
 {
     string currentStudent = name;
+
     if (currentStudent == "Sophia")
     {
-        
-        int sophiaSum = 0;
-        decimal sophiaScore;
-        foreach (int score in sophiaScores)
-        {
-            sophiaSum += score;
-        }
-        sophiaScore = (decimal)sophiaSum / currentAssignments;
-
-        Console.WriteLine("Student\t\tGrade\n");
-        Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+        studentScores = sophiaScores;
     }
+    else if (currentStudent == "Andrew")
+    {
+        studentScores = andrewScores;
+    }
+    else if (currentStudent == "Emma")
+    {
+        studentScores = emmaScores;
+    }
+    else if (currentStudent == "Logan")
+    {
+        studentScores = loganScores;
+    }
+
+    int studentSum = 0;
+    decimal currnetStudentGread = 0;
+
+    foreach (int score in studentScores)
+    {
+        studentSum += score;
+    }
+    currnetStudentGread = (decimal)studentSum / currentAssignments;
+
+    if (currnetStudentGread >= 97)
+        currentStudentletterGread = "A+";
+    if (currnetStudentGread >= 93)
+        currentStudentletterGread = "A";
+    if (currnetStudentGread >= 90)
+        currentStudentletterGread = "A-";
+    if (currnetStudentGread >= 87)
+        currentStudentletterGread = "B+";
+    if (currnetStudentGread >= 83)
+        currentStudentletterGread = "B";
+    if (currnetStudentGread >= 80)
+        currentStudentletterGread = "C+";
+    if (currnetStudentGread >= 77)
+        currentStudentletterGread = "C";
+    if (currnetStudentGread >= 97)
+        currentStudentletterGread = "A+";
+
+
+    Console.WriteLine($"{currentStudent}\t\t{currnetStudentGread}\t?");
 }
 
-/*
-
-
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
-
-Console.WriteLine("Press the Enter key to continue");
-Console.ReadLine();
-*/
