d40e8c33f666629904a005fa1ad5469dec38a30f
Tahsin <tahsinkhan12n@gmail.com>
2026-07-13 14:30:12 +0600

    new program.cs file for boolean

---
+++ csharp/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Console.WriteLine("a" != "a ");
+Console.WriteLine("a" != "A");
+Console.WriteLine(1 != 2);
+
+string myValue = "a";
+Console.WriteLine(myValue != "a");
+
+
+string value1 = "  a";
+string value2 = "A ";
+
+Console.WriteLine(value1.Trim().ToUpper() == value2.Trim().ToUpper());
+
+
+Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1);
+*/
+
+string paragram = "The quick brown fox jupms over the lazy dog";
+Console.WriteLine(paragram.Contains("fox"));
+Console.WriteLine(paragram.Contains("cow"));
---
+++ csharp/bool.cs
@@ -0,0 +1,11 @@
+var name = Console.ReadLine();
+var name_2 = Console.ReadLine();
+
+if (name == name_2)
+{
+    Console.WriteLine("Names are same");
+}
+else
+{
+    Console.WriteLine("Names are not same!");
+}
