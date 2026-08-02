f5a19ebca212c07e99d9b4955b592af7ff716d42
Tahsin <tahsinkhan12n@gmail.com>
2026-08-02 15:41:39 +0600

    some new changes

---
+++ csharp/CsharpProjects/TestProject9/Program.cs
@@ -0,0 +1,19 @@
+﻿string? readResult;
+bool validEntry = false;
+Console.Write("Enter a string containing at least three characters: ");
+do
+{
+    readResult = Console.ReadLine();
+    if (readResult != null)
+    {
+        if (readResult.Length >= 3)
+        {
+            validEntry = true;
+        }
+        else
+        {
+            Console.WriteLine("Your input is invalid, please try again.");
+        }
+    }
+} while (validEntry == false);
+
