02e1ea67fb7fdcc6ded602628d46862d6f5cf2f8
Tahsin <tahsinkhan12n@gmail.com>
2026-08-30 17:19:12 +0600

    progrees

--- csharp/Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/Program.cs
+++ csharp/Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/Program.cs
@@ -84,28 +84,78 @@ for (int i = 0; i < maxPets; i++)
 
 // display the top-level menu options
 
-Console.Clear();
-
-Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
-Console.WriteLine(" 1. List all of our current pet information");
-Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
-Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
-Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
-Console.WriteLine(" 5. Edit an animal’s age");
-Console.WriteLine(" 6. Edit an animal’s personality description");
-Console.WriteLine(" 7. Display all cats with a specified characteristic");
-Console.WriteLine(" 8. Display all dogs with a specified characteristic");
-Console.WriteLine();
-Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
-
-readResult = Console.ReadLine();
-if (readResult != null)
+do
 {
-    menuSelection = readResult.ToLower();
-}
+    Console.Clear();
 
-Console.WriteLine($"You selected menu option {menuSelection}.");
-Console.WriteLine("Press the Enter key to continue");
+    Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
+    Console.WriteLine(" 1. List all of our current pet information");
+    Console.WriteLine(" 2. Add a new animal friend to the ourAnimals array");
+    Console.WriteLine(" 3. Ensure animal ages and physical descriptions are complete");
+    Console.WriteLine(" 4. Ensure animal nicknames and personality descriptions are complete");
+    Console.WriteLine(" 5. Edit an animal’s age");
+    Console.WriteLine(" 6. Edit an animal’s personality description");
+    Console.WriteLine(" 7. Display all cats with a specified characteristic");
+    Console.WriteLine(" 8. Display all dogs with a specified characteristic");
+    Console.WriteLine();
+    Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
 
-// pause code execution
-readResult = Console.ReadLine();
+    readResult = Console.ReadLine();
+    if (readResult != null)
+    {
+        menuSelection = readResult.ToLower();
+    }
+
+    //Console.WriteLine($"You selected menu option {menuSelection}.");
+    //Console.WriteLine("Press the Enter key to continue");
+
+    // pause code execution
+    //readResult = Console.ReadLine();
+    switch (menuSelection)
+    {
+        case "1":
+            Console.WriteLine("This app feature is coming soon - check back to see the progrees");
+            Console.WriteLine("Press the Enter key to continue");
+            readResult = Console.ReadLine();
+            break;
+        case "2":
+            Console.WriteLine("This app feature is coming soon - check back to see the progrees");
+            Console.WriteLine("Press the Enter key to continue");
+            readResult = Console.ReadLine();
+            break;
+        case "3":
+            Console.WriteLine("Challenge Project - please check back soon to see progress.");
+            Console.WriteLine("Press the Enter key to continue");
+            readResult = Console.ReadLine();
+            break;
+        case "4":
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue");
+            readResult = Console.ReadLine();
+            break;
+        case "5":
+            Console.WriteLine("This app feature is coming soon - check back to see the progrees");
+            Console.WriteLine("Press the Enter key to continue");
+            readResult = Console.ReadLine();
+            break;
+        case "6":
+            Console.WriteLine("This app feature is coming soon - check back to see the progrees");
+            Console.WriteLine("Press the Enter key to continue");
+            readResult = Console.ReadLine();
+            break;
+        case "7":
+            Console.WriteLine("This app feature is coming soon - check back to see the progrees");
+            Console.WriteLine("Press the Enter key to continue");
+            readResult = Console.ReadLine();
+            break;
+        case "8":
+            Console.WriteLine("This app feature is coming soon - check back to see the progrees");
+            Console.WriteLine("Press the Enter key to continue");
+            readResult = Console.ReadLine();
+            break;
+        default:
+            break;
+        
+    }
+
+} while (menuSelection != "exit");
