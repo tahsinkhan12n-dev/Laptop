c0bfeee65a76dc4e0d6be37aeb89177faca4277c
Tahsin <tahsinkhan12n@gmail.com>
2026-09-01 23:34:24 +0600

    changes

--- csharp/CsharpProjects/TestProject12/obj/Debug/net10.0/TestProject12.AssemblyInfo.cs
+++ csharp/CsharpProjects/TestProject12/obj/Debug/net10.0/TestProject12.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject12")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b8a834c29331dee4618bd0216c7b6ce05c67441")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aee21d4decad1c23461bfc3388f8a860e0675bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject12")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject12")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
