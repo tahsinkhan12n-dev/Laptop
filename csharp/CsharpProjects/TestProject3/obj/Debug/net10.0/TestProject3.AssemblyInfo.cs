90105749886b9ee8137328ca4b2905c473c31503
Tahsin <tahsinkhan12n@gmail.com>
2026-09-08 14:13:37 +0600

    new start

--- csharp/CsharpProjects/TestProject3/obj/Debug/net10.0/TestProject3.AssemblyInfo.cs
+++ csharp/CsharpProjects/TestProject3/obj/Debug/net10.0/TestProject3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aee21d4decad1c23461bfc3388f8a860e0675bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0bfeee65a76dc4e0d6be37aeb89177faca4277c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
